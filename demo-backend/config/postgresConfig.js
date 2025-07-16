416ca8f7c969ded8bd40a0925a578aa348b44195
mightbegood12 <banunaresh1976@gmail.com>
2025-07-16 23:54:29 +0530

    [FIX] Postgres config

--- demo-backend/config/postgresConfig.js
+++ demo-backend/config/postgresConfig.js
@@ -4,19 +4,19 @@ import dotenv from "dotenv";
 dotenv.config();
 
 //local
-const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
-});
+// const pool = new Pool({
+//   user: process.env.DB_USER,
+//   host: process.env.DB_HOST,
+//   database: process.env.DB_NAME,
+//   password: process.env.DB_PASSWORD,
+//   port: process.env.DB_PORT,
+// });
 
 // SupaBase
-// const pool = new Pool({
-//   connectionString: process.env.DB_URL,
-//   ssl: { rejectUnauthorized: false },
-// });
+const pool = new Pool({
+  connectionString: process.env.DB_URL,
+  ssl: { rejectUnauthorized: false },
+});
 
 // client.query("SELECT NOW()", (err, res) => {});
 
